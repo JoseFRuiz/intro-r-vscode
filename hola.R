c5577ea17d2295d522ba573d5185cde596670704
JoseFRuiz <jfruizmu@gmail.com>
2025-05-08 10:24:52 -0500

    update

--- hola.R
+++ hola.R
@@ -10,3 +10,30 @@ print(col_means)
 col_sums <- sapply(df, sum)
 print(col_sums)
 
+rm(list = ls()) # Clear the environment
+p <- function(a, b) {
+  return ((a + b) / 2)
+}
+a <- 5
+b <- 12
+p(a, 4) -> val
+
+###################
+a <- data.frame(x=c(5, NA, 3, 4, NA),
+                y=c(1, 2, NA, 4, 5))
+is.na(a)
+
+################################
+rm(list=ls())
+
+numNAdf <- function(df){
+    return(sum(is.na(df)))
+}
+
+tabla <- data.frame(Ciudad = c('A', NA, 'C', 'D', 'E'),
+                    Sabor = c('Vainilla','Chocolate', 'Vainilla', NA, 'Fresa'),
+                    Cantidad = c(5, 10, NA, 12, NA))
+
+
+
+
